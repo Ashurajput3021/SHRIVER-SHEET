150994cc95121db5d0e815d5f8c6b1bd24744773
ashu rajput <rajputashu130@gmail.com>
2024-05-06 14:43:18 +0530

    first commit

---
+++ basic/b1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class b1{
+    public static void main(String[] args) {
+        int n,count=0;
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter any Number :");
+        n=in.nextInt();
+        while (n>0) {
+            n=n/10;
+            count++;
+        }
+        System.out.println("No of Digit :"+count);
+    }
+}
